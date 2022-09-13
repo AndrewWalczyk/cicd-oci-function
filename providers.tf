8592daaa8eaa00bdf8a1fce13a532f866254e7bd
AndrewWalczyk <86196697+AndrewWalczyk@users.noreply.github.com>
2022-09-13 16:12:39 -0400

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -29,14 +29,14 @@ provider "oci" {
   region       = var.region
 }
 
-# provider "oci" {
-#   alias        = "home-region"
-#   tenancy-ocid = var.tenancy-ocid
-#   region       = lookup(data.oci-identity-regions.home-region.regions[0], "name")
-# }
+provider "oci" {
+  alias        = "home-region"
+  tenancy-ocid = var.tenancy-ocid
+  region       = lookup(data.oci-identity-regions.home-region.regions[0], "name")
+}
 
-# provider "oci" {
-#   alias        = "current-region"
-#   tenancy-ocid = var.tenancy-ocid
-#   region       = var.region
-# }
+provider "oci" {
+  alias        = "current-region"
+  tenancy-ocid = var.tenancy-ocid
+  region       = var.region
+}
